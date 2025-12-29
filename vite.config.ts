7c1a82e3a4fc4b55dae7b2c50ce133215e919e98
James Zheng <endernoke@gmail.com>
2025-12-29 16:16:21 +0800

    holy s figma make hard-coded tailwindcss into the project instead of properly installing it

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/CodeQuestHK-2026-Website/', // Replace with your repo name
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
